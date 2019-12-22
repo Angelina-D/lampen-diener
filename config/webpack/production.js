0a01480a38254da60ed5030598c6cc77accd5917
Angelina Diener <angelinadiener@gmx.de>
2019-12-22 15:22:58 +0100

    bug solving

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -2,4 +2,13 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
 const environment = require('./environment')
 
+environment.plugins.delete('UglifyJs')
+
+environment.plugins.append(
+  'UglifyJs',
+  new UglifyJsPlugin({
+    sourceMap: true
+  })
+)
+
 module.exports = environment.toWebpackConfig()
