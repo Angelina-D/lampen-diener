a04b029ed367408a53685d966d8083f363dfdee4
Angelina Diener <angelinadiener@gmx.de>
2019-12-22 14:24:49 +0100

    final touch

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,16 +1,23 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
-// vendor/assets/javascripts directory can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file. JavaScript code in this file should be added after the last require_* statement.
-//
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
-// about supported directives.
-//
 //= require rails-ujs
-//= require activestorage
-//= require turbolinks
 //= require_tree .
+
+const overview = document.getElementById('menu-icon');
+const menu = document.getElementById('menu');
+
+overview.addEventListener("click", (event) => {
+  menu.classList.toggle("hide");
+});
+
+const switcher = document.getElementById('switch');
+const image = document.getElementById('pic-right');
+
+switcher.addEventListener("click", (event) => {
+  switcher.classList.toggle("on");
+  if(switcher.classList == "on") {
+    image.innerHTML = '<img alt="An" src="/assets/AN.png">';
+    switcher.innerText = 'AUS';
+  } else {
+    image.innerHTML = '<img alt="Aus" src="/assets/AUS-d4581088de7010b6e1d6176ebf7596935253bc8ad3803c1ae7cc5b19333f4616.png">';
+    switcher.innerText = 'AN';
+  };
+});
